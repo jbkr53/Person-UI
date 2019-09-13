1ad182547b5ab6724286e1f1c1d4c14415ad1ed6
jbkr53 <you@yourdomain.comjbkr53@mail.umkc.edu>
2019-09-13 11:20:49 -0500

    first commit

---
+++ NewPerson-UI/NewPerson-UI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewPerson-UI
+//
+//  Created by James Kirkland on 9/13/19.
+//  Copyright © 2019 James Kirkland. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ NewPerson-UI/NewPerson-UITests/NewPerson_UITests.swift
@@ -0,0 +1,34 @@
+//
+//  NewPerson_UITests.swift
+//  NewPerson-UITests
+//
+//  Created by James Kirkland on 9/13/19.
+//  Copyright © 2019 James Kirkland. All rights reserved.
+//
+
+import XCTest
+@testable import NewPerson_UI
+
+class NewPerson_UITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
